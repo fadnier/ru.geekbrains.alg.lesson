ace4c361b87ed14b1268947d7858b39852c9b9cf
fadnier <fadnier@sochidrive.org>
2020-07-28 07:02:12 +0300

    hw3

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,5 +1,8 @@
 package lesson3;
 
+import java.nio.charset.Charset;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
 //        MyStack<Integer> stack = new MyStack<>();
@@ -29,18 +32,69 @@ public class Main {
 //        }
 
 
-        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>();
+//        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>();
+//
+//        mpq.insert(5);
+//        mpq.insert(1);
+//        mpq.insert(7);
+//        mpq.insert(4);
+//        mpq.insert(2);
+//
+//        System.out.println(mpq);
+//        for (int i = 0; i < 5 ; i++) {
+//            System.out.println(mpq.remove());
+//        }
 
+        ///Zadanie 1
+        String dz1 = "Hello world";
+        System.out.println(dz1);
+        dz1 = reverseText(dz1);
+        System.out.println(dz1);
+
+        ///Zadanie 2
+        MyDeque<Integer> deq = new MyDeque<>();
+        deq.insertRight(1);
+        deq.insertRight(2);
+        deq.insertRight(3);
+        System.out.println(deq);
+        deq.insertLeft(1);
+        deq.insertLeft(2);
+        deq.insertLeft(3);
+        System.out.println(deq);
+        deq.removeLeft();
+        deq.removeRight();
+        System.out.println(deq);
+
+        ///Zadanie 3
+        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<Integer>(Comparator.reverseOrder());
         mpq.insert(5);
         mpq.insert(1);
         mpq.insert(7);
         mpq.insert(4);
         mpq.insert(2);
-
         System.out.println(mpq);
-        for (int i = 0; i < 5 ; i++) {
-            System.out.println(mpq.remove());
-        }
 
+        ///Zadanie 4
+        MyStack<Integer> stack = new MyStack<>(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        System.out.println(stack);
+    }
+
+    public static String reverseText(String string) {
+        MyStack<String> stack = new MyStack<>(string.length());
+
+        for (int i = 0; i < string.length(); i++) {
+            stack.push(string.split("")[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <string.length() ; i++) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
     }
 }
